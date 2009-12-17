97cd682c789c694fe432c3b0e49bd9c6d1a1a812
David Schmitt <david@dasz.at>
2009-12-17 12:45:19 +0000

    Implement simple MsBuild task to generate all .cst files in a project
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/ArebisCGen@17 3953f155-86ce-4baf-8dbd-25f2f86a8b97

---
+++ Arebis.CodeGeneration.MsBuild/ArebisGenTask.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Arebis.CodeGeneration.MsBuild
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using Arebis.CodeGenerator.Templated;
+
+    public class ArebisGenTask
+        : Task
+    {
+        [Required]
+        public ITaskItem[] Templates
+        {
+            get;
+            set;
+        }
+
+        [Output]
+        public ITaskItem[] GeneratedFiles
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            // Default to T3 syntax:
+            new Arebis.CodeGenerator.Templated.Syntax.T3Syntax().Setup(null);
+            var generatedFileNames = new List<string>(this.Templates.Length);
+            Log.LogMessage("Starting Arebis Generation");
+            foreach (var task in this.Templates)
+            {
+                string inputFileName = task.ItemSpec;
+                string outputFileName = Path.ChangeExtension(inputFileName, ".Designer.cs");
+                string result;
+
+                try
+                {
+                    // Build code string
+                    var builder = new CSCodeBuilder();
+                    var info = new TemplateInfo(inputFileName, new MsBuildHost(Log));
+                    builder.TemplateInfo = info;
+                    info.Parse();
+                    result = builder.CreateCode();
+
+                    try
+                    {
+                        using (var destination = new FileStream(outputFileName, FileMode.Create))
+                        {
+                            var bytes = Encoding.UTF8.GetBytes(result);
+                            destination.Write(bytes, 0, bytes.Length);
+                        }
+                        generatedFileNames.Add(outputFileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.LogError("Error while writing to [{0}]", outputFileName);
+                        Log.LogErrorFromException(ex, true, true, inputFileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.LogError("Error while compiling [{0}]", inputFileName);
+                    Log.LogErrorFromException(ex, true, true, inputFileName);
+                }
+            }
+            GeneratedFiles = generatedFileNames.Select(name => new TaskItem(name)).ToArray();
+            Log.LogMessage("Finished Arebis Generation");
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
---
+++ Arebis.CodeGeneration.MsBuild/MsBuildHost.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+
+namespace Arebis.CodeGeneration.MsBuild
+{
+    internal class MsBuildHost
+        : IGenerationHost
+    {
+        internal MsBuildHost(TaskLoggingHelper log)
+        {
+            this.Logger = log;
+        }
+
+        private readonly TaskLoggingHelper Logger;
+
+        #region IGenerationHost Members
+
+        public void CallTemplate(string templatefile, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallTemplateToFile(string templatefile, string outputfile, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NameValueCollection Settings
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void WriteOutput(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewLineString
+        {
+            get { return Environment.NewLine; }
+        }
+
+        public void Log(string fmt, params object[] args)
+        {
+            Logger.LogMessage(fmt, args);
+        }
+
+        public void WriteFile(string filename, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
