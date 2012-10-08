331b482e7e5dca07e4a2cb196178b6f9184e82e7
David Schmitt <david@dasz.at>
2012-10-08 15:30:33 +0200

    Update MsBuild references to 4.0 too

--- Arebis.CodeGeneration.CustomTool/Properties/AssemblyInfo.cs
+++ Arebis.CodeGeneration.CustomTool/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
